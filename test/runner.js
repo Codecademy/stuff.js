1021f4129b009d9308e87e10c283359b4dd29091
Amjad Masad <amjad.masad@gmail.com>
2012-11-30 14:47:33 -0500

    tests

--- test/runner.js
+++ test/runner.js
@@ -1,9 +1,47 @@
-define(function (require, exports, module) {
+define(function (require, exports) {
   'use strict';
-  var htmlFixerTest = require('test/html_fixer_test');
+  var stuff = require('lib/stuff')
+    , assert = require('vendor/assert');
+
+  var path = 'http://localhost:8888/index.html';
 
   exports.init = function () {
-    describe('htmlFixer', htmlFixerTest.init);
+    describe('Stuff', function () {
+      it('should create iframe', function (done) {
+        stuff(path, function (context) {
+          assert.ok(context);
+          done();
+        });
+      });
+
+      it('should clear all iframes', function (done) {
+        // create one more.
+        stuff(path, function (context) {
+          assert.equal(document.querySelectorAll('iframe').length, 2);
+          stuff.clear();
+          assert.equal(document.querySelectorAll('iframe').length, 0);
+          done();
+        });
+      });
+    });
+
+    describe('Context', function () {
+      var context;
+      before(function (done) {
+         stuff(path, function (c) {
+            context = c;
+            done();
+         });
+      });
+
+      it('should load some html', function (done) {
+        var testHtml = '<html><body>wat</body></html>'
+        context.load(testHtml, function () {
+          done();
+        });
+      });
+
+    });
   };
 
 });
