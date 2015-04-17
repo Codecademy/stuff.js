8b3933cde5256fd1eea8a471debf34c0869caad0
Amjad Masad <amjad.masad@gmail.com>
2015-04-16 22:33:01 -0700

    update static server code

--- dist/example/server.js
+++ dist/example/server.js
@@ -1,26 +1,13 @@
 var stat = require('node-static');
 var path = require('path');
 
-//
-// Create a node-static server instance to serve the './public' folder
-//
 var dev    = new(stat.Server)(path.join(__dirname, '../'), {cache: 0})
   , secure = new(stat.Server)(path.join(__dirname, '../secure'), {cache: 0});
 
 require('http').createServer(function (request, response) {
-    request.addListener('end', function () {
-        //
-        // Serve files!
-        //
-        dev.serve(request, response);
-    });
+  dev.serve(request, response);
 }).listen(8080);
 
 require('http').createServer(function (request, response) {
-    request.addListener('end', function () {
-        //
-        // Serve files!
-        //
-        secure.serve(request, response);
-    });
+  secure.serve(request, response);
 }).listen(8888);
--- server.js
+++ server.js
@@ -1,19 +1,11 @@
 var stat = require('node-static');
-
-//
-// Create a node-static server instance to serve the './public' folder
-//
-var dev    = new(stat.Server)('./', {cache: 0})
-  , secure = new(stat.Server)('./secure', {cache: 0});
+var dev    = new(stat.Server)('./', {cache: 0});
+var secure = new(stat.Server)('./secure', {cache: 0});
 
 require('http').createServer(function (request, response) {
-  request.addListener('end', function () {
-    dev.serve(request, response);
-  });
+  dev.serve(request, response);
 }).listen(8080);
 
 require('http').createServer(function (request, response) {
-  request.addListener('end', function () {
-    secure.serve(request, response);
-  });
+  secure.serve(request, response);
 }).listen(8888);
